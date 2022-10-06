2327f5f1dd188b9abe340a180fa95c39f5c6f806
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-10-06 21:56:27 +0100

    Create the greetings and app components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Greetings from './components/Greetings';
 
 function App() {
   return (
-    <h1>Test</h1>
+    <Routes>
+    <Route exact path="/books" element={<Greetings />} />
+    </Routes>
   );
 }
 
